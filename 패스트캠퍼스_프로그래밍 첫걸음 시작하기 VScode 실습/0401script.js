f8a6370d003ebd91149565fb5ad249a38f35a307
Seo hw <id201902@gmail.com>
2022-08-04 18:49:11 +0900

    css

--- 패스트캠퍼스_프로그래밍 첫걸음 시작하기 VScode 실습/0401script.js
+++
@@ -1,4 +0,0 @@
-
-  function showMyAlert ()
-  {alert("짠! 알림창이 나타났습니다.");}
-
--- 패스트캠퍼스_프로그래밍 첫걸음 시작하기 VScode 실습/040802javascript.js
+++
@@ -1,7 +0,0 @@
-function changeStyle () {
-var newStyle = {};
-newStyle.attribute = prompt("어떤 스타일을 변경하시겠어요?");
-newStyle.value = prompt("어떤 값을 입력하시겠어요?");
-console.log(newStyle);
-document.querySelector("div").style[newStyle.attribute] = newStyle.value;
-}
