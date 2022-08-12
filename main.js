3651840a105a14b944f7a9c9aa26ea1c75e4d598
Seo hw <id201902@gmail.com>
2022-08-12 22:48:56 +0900

    Home_Contact me (자바스크립트)

--- main.js
+++ main.js
@@ -21,7 +21,17 @@ document.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+  scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homecontactBtn = document.querySelector('.home__contact');
+homecontactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
